ff846a59a9c4a1718b98da86b5db40b1008e3329
X3D <2172738590@qq.com>
2025-03-06 20:06:23 +0800

    LiLaoBan

---
+++ day1fs.js
@@ -0,0 +1,37 @@
+// 弹出警告框
+// alert('晚安');
+// 要求用户输入
+// prompt('请告诉我你是谁?');
+// 控制台输出
+// console.log('仅你可见');
+//var xxx  代表声明某个变量,xxx是自行命名的变量名
+/* var a;
+a = 99;
+console.log(a); */
+/* var a = prompt('输入你的姓名:');
+var b = prompt('输入你的年龄:');
+var c = prompt('你的电子邮箱是?');
+var d = prompt('你的家庭住址是?');
+var e = prompt('你的工资是?');
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e); */
+/* var a =prompt('请输入您的姓名:');
+alert(a); */
+/* var a = 10;
+var b = 99;
+alert('开始时a的值:' + a);
+alert('开始时b的值:' + b);
+alert('执行交换......');
+alert('交换完成');                              
+var c;
+c = a;
+a = b;                                        
+b = c;
+alert('交换后a的值:' + a);
+alert('交换后b的值:' + b); */
+//isNan()函数用于检查其参数是否是非数字值,是返回false,否则返回true  NaN是非数字值
+/* console.log(isNaN(123)); //false
+console.log(isNaN('123')); //True */
